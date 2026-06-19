3f0355aa90c71da54586447a2c4bcead7563d27d
rauldzmartin <rauldzmartin@gmail.com>
2026-06-19 11:40:00 +0200

    chore: bump version to v0.6.26-beta

--- server/app.py
+++ server/app.py
@@ -69,7 +69,7 @@ except Exception as e:
     ExternalError = Exception  # type: ignore
     _import_error = e
 
-app = FastAPI(title="PoB HTTP API", version="0.5g")
+app = FastAPI(title="PoB HTTP API", version="0.6.26-beta")
 
 app.add_middleware(
     CORSMiddleware,
